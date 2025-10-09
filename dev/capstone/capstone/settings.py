b414b15e3e55d01c7bb3c7156978a1f6f6fe13ca
Danyel Pereira <dany.pereira@duocuc.cl>
2025-10-09 18:05:18 -0300

    rutas arregladas

--- dev/capstone/capstone/settings.py
+++ dev/capstone/capstone/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'agendabarber.urls'
+ROOT_URLCONF = 'capstone.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'agendabarber.wsgi.application'
+WSGI_APPLICATION = 'capstone.wsgi.application'
 
 
 # Database
